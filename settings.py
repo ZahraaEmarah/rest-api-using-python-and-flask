ea3763b124f0db26fcb6a344e8bc1f2aa9fd4b17
ZahraaEmarah <zahraa.emara@syngenta.com>
2022-10-27 10:13:51 +0200

    cors implementation

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/zahraa.emara/Desktop/rest_api_using_python_and_flask/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
